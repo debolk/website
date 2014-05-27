1b93ddf549b4e2dff9a95cdfef798dcea07f0ca7
Yvonnevonne <yhaartsen@gmail.com>
2014-05-27 12:56:55 +0200

    slideshow werkt alleen lokaal!

--- js/app.js
+++ js/app.js
@@ -4,4 +4,25 @@ $(document).ready(function(){
         trigger: 'hover',
         placement: 'top',
     });
+	//Slideshow
+	$(function() {function slideSwitch() {
+	var $active = $('#slideshow div.active');
+
+		if ( $active.length == 0 ) $active = $('#slideshow div:last');
+
+	var $next = $active.next().length ? $active.next()
+		: $('#slideshow div:first');
+
+		$active.addClass('last-active');
+
+		$next.css({opacity: 0.0})
+			.addClass('active')
+			.animate({opacity: 1.0}, 1000, function() {
+				$active.removeClass('active last-active');
+			});
+	}
+	$(function (){
+		setInterval(slideSwitch, 5000);
+	});
+	});
 });
